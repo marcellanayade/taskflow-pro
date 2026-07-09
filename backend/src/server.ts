3ed8ba06ce241a431ea0ce64429750f3657c311c
Marcella Santiago <nayademarcella@gmail.com>
2026-07-09 12:08:30 -0300

    chore: initial setup of the backend with TypeScript and Express

---
+++ backend/src/server.ts
@@ -0,0 +1,16 @@
+import express, { Request, Response } from 'express';
+
+const app = express();
+
+const PORT = 5000;
+
+app.get('/', (req: Request, res: Response) => {
+  return res.json({ 
+    message: "Server running..." 
+  });
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT} at: http://localhost:${PORT}`);
+});
+
